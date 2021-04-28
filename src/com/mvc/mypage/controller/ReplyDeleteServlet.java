a853b04305928e447dbf304d474704da79aad8f6
heeejoo0518 <heeejoo0518@gmail.com>
2021-04-28 11:13:08 +0900

    게시판-글/댓글 서블릿 작업시작

--- src/com/mvc/mypage/controller/ReplyDeleteServlet.java
+++ src/com/mvc/mypage/controller/ReplyDeleteServlet.java
@@ -7,19 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet(urlPatterns={"/mypage/mypost","/mypage"})
-public class BoardDeleteServlet extends HttpServlet {
+@WebServlet(urlPatterns="/mypage/myreply")
+public class ReplyDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
-    public BoardDeleteServlet() {
-       
+
+    public ReplyDeleteServlet() {
+        super();
     }
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
